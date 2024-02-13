4d97223b7a62233f30f0af15a2addc80a13b1376
joba <jobtennis21@gmail.com>
2024-02-13 23:25:30 +0300

    version 3 with api done

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,92 @@
+var styleElement = document.createElement("style");
 
+var style = `
 
-var styleElement = document.createElement('style');
+.hidden {
+  display: none;
+}
 
+.block {
+  display: block;
+}
 
-var keyframes = `
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.spinning {
+  animation: spin 1s linear infinite; /* Adjust the duration and timing function as per your needs */
+}
+.toast-progress {
+
+    position: absolute;
+
+    display: block;
+
+    bottom: 0;
+
+    left: 0;
+
+    height: 4px ;
+
+    width: 100%;
+
+    background: #b7b7b7;
+
+    animation: toastProgress 3s ease-in-out forwards;
+}
+
+.toast.toast-success {
+
+    background: #95eab8;
+}
+.toast.toast-success .toast-progress {
+
+    background-color: red;
+}
+.toast.toast-danger {
+
+    background: #efaca5;
+}
+.toast.toast-danger .toast-progress {
+
+    background-color: #e74c3c;
+}
+.toast {
+    height: auto;
+
+    font-size: 16px;
+
+    position: fixed;
+
+    top: 25px;
+
+    right: 25px;
+
+    max-width: 300px;
+
+    background: #fff;
+
+    padding: 1rem;
+
+    border-radius: 4px;
+
+    box-shadow: -1px 1px 10px rgba(0, 0, 0, 0.3);
+
+    z-index: 2147483647;
+
+    animation: slideInRight 0.3s ease-in-out forwards,
+
+        fadeOut 0.5s ease-in-out forwards 3s;
+
+    transform: translateX(110%);
+}
+
     @keyframes slideInRight {
         0% {
             transform: translateX(110%);
@@ -47,83 +130,68 @@ var keyframes = `
     }
 `;
 
-
-styleElement.innerHTML = keyframes;
-
+styleElement.innerHTML = style;
 
 document.head.appendChild(styleElement);
-
 
 var GFont = document.createElement("link");
 GFont.rel = "stylesheet";
 GFont.href =
-  "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"; 
+  "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
 document.head.appendChild(GFont);
-
-
-
 
 var toastContainer = document.createElement("div");
 
-toastContainer.style.zIndex = "100"; 
+toastContainer.style.zIndex = "100";
 
+let loader = document.createElement("div");
+loader.style.cssText =
+  "z-index:2147483647; position: fixed;background-color: #4299e1;width: 2.5rem; height: 2.5rem; right: 0; top: 0;";
+loader.classList.add('hidden')
+loader.innerHTML = ` <svg
+                    class="spinning"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      opacity="0.2"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                      fill="#FFFFFF"
+                    />
+                    <path
+                      d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
+                      fill="#FFFFFF"
+                    /></svg>`;
 
-var toast = document.createElement("div");
 toastContainer.id = "toast-overlay";
 toastContainer.classList.add("toast-overlay");
 
-
-
+document.body.appendChild(loader);
 document.body.appendChild(toastContainer);
 
+let icon = {
+  success: '<span class="material-symbols-outlined">✔</span>',
 
+  danger: '<span class="material-symbols-outlined">⚠︎</span>',
+};
 
+const showToast = (
+  message = "Sample Message",
 
+  toastType = "info",
 
-  
-let icon = { 
+  duration = 5000
+) => {
+  if (!Object.keys(icon).includes(toastType)) toastType = "info";
 
-    success: 
+  let box = document.createElement("div");
 
-    '<span class="material-symbols-outlined">task_alt</span>', 
+  box.classList.add("toast", `toast-${toastType}`);
 
-    danger: 
-
-    '<span class="material-symbols-outlined">error</span>', 
-
-    warning: 
-
-    '<span class="material-symbols-outlined">warning</span>', 
-
-    info: 
-
-    '<span class="material-symbols-outlined">info</span>', 
-}; 
-
-  
-const showToast = ( 
-
-    message = "Sample Message", 
-
-    toastType = "info", 
-
-    duration = 5000) => { 
-
-    if ( 
-
-        !Object.keys(icon).includes(toastType)) 
-
-        toastType = "info"; 
-
-  
-
-    let box = document.createElement("div"); 
-
-  //box.classList.add( "toast", `toast-${toastType}`);
-  
-  box.style.cssText = ' position: fixed; top: 25px; right: 25 max-width: 300px; background: #fff; padding: 0.5rem; border-radius: 4px; box-shadow: -1px 1px 10px rgba(0, 0, 0, 0.3); z-index: 1023; animation: slideInRight 0.3s ease-in-out forwards, fadeOut 0.5s ease-in-out forwards 3s; transform: translateX(110%);'
-
-    box.innerHTML = ` <div class="toast-content-wrapper" style="background-color: #95eab8; display: flex; justify-content: space-between; align-items: center;"> 
+  box.innerHTML = ` <div class="toast-content-wrapper" style="z-index: 2147483647; display: flex; justify-content: space-between; align-items: center;"> 
 
                       <div class="toast-icon" style=" padding: 0.35rem 0.5rem; font-size: 1.5rem;"> 
 
@@ -131,45 +199,26 @@ const showToast = (
 
                       </div> 
 
-                      <div class="toast-message" style="flex: 1; font-size: 0.9rem; color: #000000; padding: 0.5rem;">${message}</div> 
+                      <div class="toast-message" style="flex: 1; font-size: 16px; color: #000000; padding: 0.5rem;">${message}</div> 
 
-                      <div class="toast-progress" style="position: absolute; display: block; bottom: 0; left: 0; height: 4px; width: 100%; background: #b7b7b7; animation: toastProgress 3s ease-in-out forwards;"></div> 
+                      <div class="toast-progress" ></div> 
 
-                      </div>`; 
+                      </div>`;
 
-    duration = duration || 5000; 
+  duration = duration || 5000;
 
-    box.querySelector(".toast-progress").style.animationDuration = 
+  box.querySelector(".toast-progress").style.animationDuration = `${
+    duration / 1000
+  }s`;
 
-            `${duration / 1000}s`; 
+  let toastAlready = document.body.querySelector(".toast");
 
-  
+  if (toastAlready) {
+    toastAlready.remove();
+  }
 
-    let toastAlready =  
-
-        document.body.querySelector(".toast"); 
-
-    if (toastAlready) { 
-
-        toastAlready.remove(); 
-
-    } 
-
-  
-
-  document.body.appendChild(box)
-
-}; 
-  
-
-
- 
-
-
-function hide() {
-  console.log("hello hide");
-  toast.classList.replace("show", "hide");
-}
+  document.body.appendChild(box);
+};
 
 var myDiv = document.querySelector("div");
 const list = [
@@ -180,26 +229,56 @@ const list = [
   "www.danielcobb.co.uk",
 ];
 
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
 chrome.storage.local.get("featureEnabled", function (data) {
   var featureEnabled = data.featureEnabled || false;
 
   if (featureEnabled) {
     console.log("enabled");
     toastContainer.classList.add(
-  "position-fixed",
-  "top-0",
-  "end-0",
-  "p-3",
-  "bg-primary"
-);
+      "position-fixed",
+      "top-0",
+      "end-0",
+      "p-3",
+      "bg-primary"
+    );
     chrome.runtime.sendMessage(
       { action: "getCurrentUrl" },
-      function (response) {
-        console.log("Current domain:", response.domain);
-        if (list.includes(response.domain)) {
-         
-        } else {
-         showToast("Article Submitted Successfully","success",5000); 
+      async function (response) {
+        try {
+          loader.classList.replace('hidden', 'block');
+          const data = await fetchData(
+            "https://prospectregistry.com/api/v1/domain/get"
+          );
+          let lists = data?.domains.map((list) => {
+            return list.domain_url;
+          });
+          if (lists?.includes(response.domain)) {
+            loader.classList.replace('block', 'hidden');
+            showToast("You have seen this page before", "danger", 5000);
+          } else {
+            loader.classList.replace('block', 'hidden');
+            showToast("You have not visited this page before", "success", 5000);
+          }
+        } catch (error) {
+          console.error("Error:", error);
         }
       }
     );
--- script.js
+++ script.js
@@ -1,47 +1,76 @@
 const powerButton = document.getElementById('powerButton');
-
+const addButton = document.getElementById('addButton');
 powerButton.addEventListener('click', function() {
-    // Toggle the power button's state
     this.classList.toggle('power-on');
-
-    chrome.storage.local.get('featureEnabled', function(data) {
+    chrome.storage?.local.get('featureEnabled', function(data) {
             var featureEnabled = data.featureEnabled || false;
             chrome.storage.local.set({ 'featureEnabled': !featureEnabled });
+    }); 
+});
 
-  console.log("clicked from script js")
-    });
- 
-    
+const RegisterDomain = (domain) => {
+    document.getElementById('spinner').classList.replace('hidden', 'block');
+     document.getElementById('button-text').classList.replace('block', 'hidden');
+const url = "https://prospectregistry.com/api/v1/domain/add";
+      const data = {
+        domain_url: domain,
+      };
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+
+      fetch(url, options)
+        .then((data) => {
+            console.log("Success:", data);
+             document.getElementById('spinner').classList.replace('block', 'hidden');
+            document.getElementById('button-text').classList.replace('hidden', 'block');
+            document.getElementById('message').classList.replace('text-black', 'text-green-500');
+            document.getElementById('message').classList.replace('hidden', 'flex');
+              document.getElementById('message').textContent = 'Domain added successfully '
+            setTimeout(() => {
+                document.getElementById('message').classList.replace('flex', 'hidden');
+            }, 5000)
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+             document.getElementById('spinner').classList.replace('block', 'hidden');
+            document.getElementById('button-text').classList.replace('hidden', 'block');
+            document.getElementById('message').classList.replace('text-black', 'text-red-500');
+            document.getElementById('message').classList.replace('hidden', 'flex');
+             document.getElementById('message').textContent = 'Domain adding not sucessfull'
+             setTimeout(() => {
+                document.getElementById('message').classList.replace('flex', 'hidden');
+            }, 5000)
+        });
+}
+
+
+
+chrome.runtime.sendMessage({ action: "getCurrentUrl" }, function (response) {
+    if (chrome.runtime.lastError) {
+        console.error("Errorrrrrrrr:", chrome.runtime.lastError.message);
+    } else {
+        addButton.addEventListener('click', function() {
+            console.log("add to DB", response.domain)
+            RegisterDomain(response.domain)
+        })
+    }
 });
 
 
    
-chrome.storage.local.get('featureEnabled', function(data) {
+chrome.storage?.local.get('featureEnabled', function(data) {
     var featureEnabled = data.featureEnabled || false;
-
     if (featureEnabled) {
-    
         console.log("enabled")
         powerButton.classList.toggle('power-on');
     } else {
-      
         console.log("disabled")
        
     }
 });
 
-/*
-    document.getElementById('toggleFeatureBtn').addEventListener('click', function() {
-        
-      
-
-
-        chrome.storage.local.get('featureEnabled', function(data) {
-            var featureEnabled = data.featureEnabled || false;
-            chrome.storage.local.set({ 'featureEnabled': !featureEnabled });
-
-  console.log("clicked from script js")
-        });
-    });
-
-    */
