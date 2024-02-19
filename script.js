64c4c83328c05ffcc8a1217242ca8bbc2d839e2b
joba <jobtennis21@gmail.com>
2024-02-19 20:15:57 +0300

    trying to js alert

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
+// Get references to the power button and add button elements
 const powerButton = document.getElementById("powerButton");
 const addButton = document.getElementById("addButton");
+
+// Event listener for the power button
 powerButton.addEventListener("click", function () {
+  // Toggle the 'power-on' class on the power button
   this.classList.toggle("power-on");
-  chrome.storage?.local.get("featureEnabled", function (data) {
-    var featureEnabled = data.featureEnabled || false;
-    chrome.storage.local.set({ featureEnabled: !featureEnabled });
-  });
+
+  // Check if the chrome.storage.local API is available
+  if (chrome.storage?.local) {
+    // Retrieve the 'featureEnabled' data from local storage
+    chrome.storage.local.get("featureEnabled", function (data) {
+      // Get the current state of the feature or default to false
+      var featureEnabled = data.featureEnabled || false;
+      // Update the feature status in local storage
+        chrome.storage.local.set({ featureEnabled: !featureEnabled });
+    });
+    //alert('refresh the page')
+  }
 });
 
+// Function to register a domain
 const RegisterDomain = (domain) => {
+  // Show spinner and hide button text
   document.getElementById("spinner").classList.replace("hidden", "block");
   document.getElementById("button-text").classList.replace("block", "hidden");
+
+  // API endpoint for adding a domain
   const url = "https://prospectregistry.com/api/v1/domain/add";
+  // Data payload
   const data = {
     domain_url: domain,
   };
+  // Fetch options
   const options = {
     method: "POST",
     headers: {
@@ -23,13 +41,14 @@ const RegisterDomain = (domain) => {
     body: JSON.stringify(data),
   };
 
+  // Make a POST request to add the domain
   fetch(url, options)
     .then((data) => {
       console.log("Success:", data);
+      // Hide spinner and show button text
       document.getElementById("spinner").classList.replace("block", "hidden");
-      document
-        .getElementById("button-text")
-        .classList.replace("hidden", "block");
+      document.getElementById("button-text").classList.replace("hidden", "block");
+      // Send message to content script to indicate domain addition success
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "domainAddSuccess" });
         window.close();
@@ -37,31 +56,37 @@ const RegisterDomain = (domain) => {
     })
     .catch((error) => {
       console.error("Error:", error);
+      // Hide spinner and show button text
       document.getElementById("spinner").classList.replace("block", "hidden");
-      document
-        .getElementById("button-text")
-        .classList.replace("hidden", "block");
+      document.getElementById("button-text").classList.replace("hidden", "block");
+      // Send message to content script to indicate domain addition failure
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "domainAddFail" });
       });
     });
 };
 
+// Get the current URL using chrome.runtime API
 chrome.runtime.sendMessage({ action: "getCurrentUrl" }, function (response) {
   if (chrome.runtime.lastError) {
-    console.error("Errorrrrrrrr:", chrome.runtime.lastError.message);
+    // Handle errors
+    console.error("Error:", chrome.runtime.lastError.message);
   } else {
+    // Event listener for the add button
     addButton.addEventListener("click", function () {
       console.log("add to DB", response.domain);
+      // Register the domain
       RegisterDomain(response.domain);
     });
   }
 });
 
+// Check if the feature is enabled from local storage
 chrome.storage?.local.get("featureEnabled", function (data) {
   var featureEnabled = data.featureEnabled || false;
   if (featureEnabled) {
     console.log("enabled");
+    // Toggle the 'power-on' class on the power button if feature is enabled
     powerButton.classList.toggle("power-on");
   } else {
     console.log("disabled");
